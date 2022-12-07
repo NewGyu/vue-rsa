052ac9a297dddcd4f94e4aed8a7165e0bacb3357
NewGyu <newgyu+git@kinoboku.net>
2022-12-07 05:49:45 +0000

    FIX: for gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
+        baseURL: "/vue-rsa",
         head: {
             title: "An example",
             link: [
